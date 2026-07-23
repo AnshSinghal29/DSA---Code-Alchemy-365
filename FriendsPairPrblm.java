8807d8fa2e2cd68251e2e9b89d2eb0996d98b3c8
anshhsinghal <singhalansh121@gmail.com>
2026-07-23 14:23:34 +0530

    Add FriendsPairPrblm.java

---
+++ FriendsPairPrblm.java
@@ -0,0 +1,23 @@
+public class FriendsPairPrblm {
+     public static int FriendPairingProblem(int n) {
+          // BC
+          if (n == 1 || n == 2) {
+               return n;
+          }
+          // choice
+          // single
+          int fnm1 = FriendPairingProblem(n - 1);
+
+          // pair
+          int fnm2 = FriendPairingProblem(n - 2);
+          int pairWays = (n - 1) * fnm2;
+
+          // total ways
+          int totWay = fnm1 + pairWays;
+          return totWay;
+     }
+
+     public static void main(String[] args) {
+          System.out.println(FriendPairingProblem(3));
+     }
+}
