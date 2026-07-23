4419aec8a767ef00207fef43748cd42d38fd6db5
anshhsinghal <singhalansh121@gmail.com>
2026-07-23 14:23:35 +0530

    Add STRINGG.java

---
+++ STRINGG.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class STRINGG {
+
+     public static void main(String args[]){
+          // String str1 = "abcd";
+          // String str2 = new String("xyz");
+
+          Scanner sc = new Scanner(System.in);
+          String name;
+          name = sc.nextLine();
+          System.out.println(name);
+
+          String fullname = "Tony Stark";
+          System.out.println(fullname.length());
+     }
+}
