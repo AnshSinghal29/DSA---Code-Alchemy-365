d4d76e42144592f30b5fb0f9939c2c877304b79b
anshhsinghal <singhalansh121@gmail.com>
2026-07-23 14:23:38 +0530

    Add javaBasic.java

---
+++ javaBasic.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class javaBasic {
+     public static void main(String args[]){
+          // System.out.print("Hello World!");
+          // System.out.println("Mai hu Giaan");
+          // System.out.print("Mai hu bada takatwar\n");
+
+           // basic pattern
+           
+          // System.out.println("****");
+          // System.out.println("***");
+          // System.out.println("*");
+
+          
+          // ye keval input lene ke liye humne ek scanner object import kia and sc.nect me input stored hai
+          // Scanner sc = new Scanner(System.in);
+          // String input = sc.next();
+          // System.out.println(input);
+
+          // spaces agr input krni h string ki toh...
+
+          Scanner sc = new Scanner(System.in);
+          String input = sc.nextLine();
+          System.out.println(input);
+
+
+     }
+}
