27336aa1ad1684ca9f22b1a5011afb5de1d346f6
anshhsinghal <singhalansh121@gmail.com>
2026-07-23 14:23:33 +0530

    Add CustomString.java

---
+++ CustomString.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class CustomString {
+
+     public static void main(String args[]) {
+          // String str1 = "abcd";
+          // String str2 = new String("xyz");
+
+          Scanner sc = new Scanner(System.in);
+          String name;
+          name = sc.next();
+          System.out.println(name);
+     }
+}
