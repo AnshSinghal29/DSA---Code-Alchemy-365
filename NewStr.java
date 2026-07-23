b0639c440ee50431d92fe34c3bc3531af36db4dd
anshhsinghal <singhalansh121@gmail.com>
2026-07-23 14:23:35 +0530

    Add NewStr.java

---
+++ NewStr.java
@@ -0,0 +1,11 @@
+import java.lang.String;
+import java.util.*;
+public class NewStr{
+     public static void main(String args[]){
+          Scanner sc = new Scanner(System.in);
+          System.out.println("enter your name");
+           java.lang.String name=sc.nextLine();
+          sc.close();
+
+     }
+}
