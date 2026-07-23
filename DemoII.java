e817b52db1ffff8885a760b83da1e8a9c3e8afe3
anshhsinghal <singhalansh121@gmail.com>
2026-07-23 14:23:34 +0530

    Add DemoII.java

---
+++ DemoII.java
@@ -0,0 +1,14 @@
+import java.net.SocketPermission;
+import java.util.*;
+
+public class DemoII {
+     public static void main(String args[]){
+          Scanner sc = new Scanner(System.in);
+          System.out.println("ENTER N");
+          int n = sc.nextInt();
+          Scanner sc = new Scanner(System.in);
+          System.out.println("ENTER k");
+          int k = sc.nextInt();
+          int s
+     }
+}
